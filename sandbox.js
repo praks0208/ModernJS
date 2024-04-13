10693201f983b79783527ca589eca54531aa00c1
praks0208 <praks.sawant26@gmail.com>
2024-04-13 19:58:44 +0530

    Object

--- sandbox.js
+++ sandbox.js
@@ -305,3 +305,97 @@ ul.innerHTML = html;
 
 
 
+OBJECTS:
+// Object Literal
+
+
+let user = {
+    name:'crystal',
+    age:30,
+    email:'crystal@gmail.com',
+    location:'berlin',
+    blogs: ['why mac & cheese rules','10 things to make with marmite']
+};
+
+
+console.log(user);
+console.log(user.name);
+
+
+// user.age = 35;
+console.log(user.age);
+
+
+console.log(user['name']); //square bracket notation
+user['name'] = 'chun-li';
+console.log(user['name']);
+
+
+console.log(typeof(user));
+// Oject within an object
+let user = {
+    name:'crystal',
+    age:30,
+    email:'crystal@gmail.com',
+    location:'berlin',
+    blogs: [
+        {title: 'why mac & cheese rules', likes:30},
+        {title: '10 things to make with marmite', likes:50}
+    ],
+    login(){
+        console.log('the user logged in');
+    },
+    logout(){
+        console.log('the user logged out');
+    },
+    logBlogs(){
+        // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog=>{
+            console.log(blog.title,blog.likes);
+        });
+    }
+};
+
+
+user.login();
+user.logout();
+user.logBlogs();
+
+// Math object
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+
+const area = 7.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+
+// random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random*100000));
+
+// Primitive values
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+
+console.log(`scoreOne:${scoreOne}`,`scoreTwo: ${scoreTwo}`);
+
+
+scoreOne = 100;
+console.log(`scoreOne:${scoreOne}`,`scoreTwo: ${scoreTwo}`);
+
+
+// Reference values
+const userOne = {name:'ryu',age:30};
+userTwo = userOne;
+console.log(userOne,userTwo);
+userOne.name='chun-li';
+console.log(userOne,userTwo);
+
